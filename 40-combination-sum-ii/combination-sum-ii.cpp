a6d4ba3887adc42289e126f31db56eb1bccc8399
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-06-10 11:23:19 +0530

    Time: 8 ms (28.18%) | Memory: 14.1 MB (21.48%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-    // vector<vector<int>> ans;
-    // void helper(vector<int>& candidates, int target,int i,int currSum, vector<int> temp){
-    //     if(currSum>target) return; //bounding condition
-    //     if(i==candidates.size()){ //base case
-    //         if(currSum==target) ans.push_back(temp);
-    //         return;
-    //     }
-    //     currSum+=candidates[i+1];//Inclusion
-    //     temp.push_back(candidates[i]);
-    //     helper(candidates,target,i,currSum,temp);
-    //     currSum-=candidates[i+1];
-    //     temp.pop_back();
-    //     helper(candidates,target,i+1,currSum,temp);//Exclusion
-    // }
     vector<vector<int>> ans;
     void helper(vector<int>& candidates, int target, int ind,vector<int> temp){
         if(target==0){
@@ -30,9 +16,6 @@ public:
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        // vector<int> temp;
-        // helper(candidates,target,0,0,temp);
-        // return ans;
         vector<int> temp;
         sort(candidates.begin(),candidates.end());
         helper(candidates,target,0,temp);
