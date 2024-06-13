6451a4afd9cfffac615f057aefc941a1c0d3f245
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-06-13 17:34:56 +0530

    Time: 4 ms (79.59%) | Memory: 11.9 MB (56.25%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ones = 0, twos = 0;
-        for (int i=0;i<nums.size();i++) {
-            // If num is seen a second time, add it to `twos`
-            twos= twos |(ones & nums[i]);
-            // If num is seen a first time, add it to `ones`
-            ones =ones^ nums[i];
-            // If num is seen a third time, remove it from `ones` and `twos`
-            int threes = ones & twos;
-            ones &= ~threes;
-            twos &= ~threes;
+        int seenOnce=0, seenTwice=0;
+        for(int i=0;i<nums.size();i++){ 
+            seenOnce=(seenOnce ^ nums[i]) & (~seenTwice);// If seenOnce so I can't do seen Twice
+            seenTwice=(seenTwice ^ nums[i]) & (~seenOnce);// If seenTwice so I can't do seenOnce because that number is removed from seenOnce and added to seenTwice 
+        // If occured one more time (i.e seenThrice) so seenOnce will become zero
         }
-        return ones;
-
+        return seenOnce;
     }
 };
