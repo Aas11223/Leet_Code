5edb256165151f8c9e1ebb819dbc232995e156d1
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-06-15 11:44:07 +0530

    Time: 4 ms (5.85%) | Memory: 8.4 MB (31.98%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-         int max_ele=arr[0];
-         int chunk=0;
-         for(int i=0;i<arr.size();i++){
+        //  int max_ele=arr[0];
+        //  int chunk=0;
+        //  for(int i=0;i<arr.size();i++){
+        //     max_ele=max(max_ele,arr[i]);
+        //     if(max_ele==i) chunk++;
+        //  }
+        //  return chunk;
+        int max_ele=arr[0];
+        int count=0;
+        for(int i=0;i<arr.size();i++){
             max_ele=max(max_ele,arr[i]);
-            if(max_ele==i) chunk++;
-         }
-         return chunk;
+            if(max_ele==i) count++;
+        }
+        return count;
     }
 };
