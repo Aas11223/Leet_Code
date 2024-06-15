7047ff3c10fe2f6966887b8c39cd68ce7e67c2aa
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-06-15 11:46:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (31.98%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -8,11 +8,13 @@ public:
         //     if(max_ele==i) chunk++;
         //  }
         //  return chunk;
-        int max_ele=arr[0];
         int count=0;
-        for(int i=0;i<arr.size();i++){
-            max_ele=max(max_ele,arr[i]);
-            if(max_ele==i) count++;
+        // vector<int>pref(arr.size());
+        // pref[0]=arr[0];
+        if(arr[0]==0)count++;
+        for(int i=1;i<arr.size();i++){
+            arr[i]=max(arr[i-1],arr[i]);
+            if(arr[i]==i)count++;
         }
         return count;
     }
