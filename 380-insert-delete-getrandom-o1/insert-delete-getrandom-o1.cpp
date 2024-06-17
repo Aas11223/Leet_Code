63cb7293e6b1173a42fe5131b69162fc3df16036
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-06-17 10:40:12 +0530

    Time: 171 ms (22.53%) | Memory: 100.8 MB (18.73%) - LeetSync

---
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -0,0 +1,40 @@
+class RandomizedSet {
+private:
+    std::unordered_map<int, int> valueIndexMap; // Maps value to its index in the vector
+    std::vector<int> values; // Stores the elements
+
+public:
+    RandomizedSet() {
+        // Constructor
+    }
+    
+    bool insert(int val) {
+        if (valueIndexMap.find(val) != valueIndexMap.end()) {
+            return false; // Value already exists
+        }
+        valueIndexMap[val] = values.size(); // Map value to current index
+        values.push_back(val); // Add value to the end of the vector
+        return true;
+    }
+    
+    bool remove(int val) {
+        if (valueIndexMap.find(val) == valueIndexMap.end()) {
+            return false; // Value does not exist
+        }
+        int lastElement = values.back(); // Get the last element in the vector
+        int index = valueIndexMap[val]; // Get index of the element to remove
+        
+        values[index] = lastElement; // Replace the element to remove with the last element
+        valueIndexMap[lastElement] = index; // Update index of the last element in the map
+        
+        values.pop_back(); // Remove the last element
+        valueIndexMap.erase(val); // Remove the element from the map
+        
+        return true;
+    }
+    
+    int getRandom() {
+        int randomIndex = rand() % values.size(); // Generate a random index
+        return values[randomIndex]; // Return the value at the random index
+    }
+};
