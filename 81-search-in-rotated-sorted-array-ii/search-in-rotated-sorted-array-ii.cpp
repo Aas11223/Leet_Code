22552579c590c4db129906f2cad14c2c0385f2de
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-06-19 10:27:10 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (31.58%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         for(int i=0;i<nums.size();i++){
-            if(target==nums[i]) return true;
+            if(nums[i]==target) return true;
         }
         return false;
     }
