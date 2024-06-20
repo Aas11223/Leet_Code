0a3e38e45cd462e025dc42ca164f5f937bbdcbb2
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-06-20 19:29:18 +0530

    Time: 12 ms (9.51%) | Memory: 10.5 MB (99.78%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,20 +1,37 @@
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//        vector<int> ans(nums1.size());
+//        bool found=false;
+//        for(int i=0;i<nums1.size();i++){
+//         found=false;
+//            for(int j=0;j<nums2.size();j++){
+//                if(nums1[i]==nums2[j]) found =true;
+//                if(found && nums2[j]>nums1[i]){
+//                 ans[i]=nums2[j];
+//                 break;
+//                }
+//            }
+//        }
+//        for(int i=0;i<ans.size();i++){
+//         if(ans[i]==0) ans[i]=-1;
+//        }
+//        return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-       vector<int> ans(nums1.size());
-       bool found=false;
-       for(int i=0;i<nums1.size();i++){
-        found=false;
-           for(int j=0;j<nums2.size();j++){
-               if(nums1[i]==nums2[j]) found =true;
-               if(found && nums2[j]>nums1[i]){
-                ans[i]=nums2[j];
-                break;
+       vector<int> ans(nums1.size(), -1); // Initialize ans with size of nums1 and default value -1
+       for(int i=0; i<nums1.size(); i++){
+           bool found = false;
+           for(int j=0; j<nums2.size(); j++){
+               if(nums1[i] == nums2[j]) found = true;
+               if(found && nums2[j] > nums1[i]){
+                   ans[i] = nums2[j];
+                   break;
                }
            }
-       }
-       for(int i=0;i<ans.size();i++){
-        if(ans[i]==0) ans[i]=-1;
        }
        return ans;
     }
