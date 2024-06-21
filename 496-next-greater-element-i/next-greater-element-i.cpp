50fc11a59527420c9d3e72673e86c24f39238bef
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-06-21 10:42:49 +0530

    Time: 7 ms (38.80%) | Memory: 10.7 MB (97.81%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -27,7 +27,7 @@ public:
            bool found = false;
            for(int j=0; j<nums2.size(); j++){
                if(nums1[i] == nums2[j]) found = true;
-               if(found && nums2[j] > nums1[i]){
+               if(found==true && nums2[j] > nums1[i]){
                    ans[i] = nums2[j];
                    break;
                }
