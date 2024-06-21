973fce8d5a35ba0798b4321a71e6780f88397517
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-06-21 11:08:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (60.67%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -17,6 +17,16 @@ public:
             }
         }
         return st.empty();
-        
+        // stack<char> st;
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]=='(' || s[i]=='[' || s[i]=='{') st.push(s[i]);
+        //     else{
+        //         if(!st.empty()){
+        //             if((s[i]==')' && st.top()=='(') || (s[i]==']' && st.top()=='[') || (s[i]=='}' && st.top()=='{')) st.pop();
+        //             else return false;
+        //         }else return false;
+        //     }
+        // }
+        // return st.empty();
     }
 };
