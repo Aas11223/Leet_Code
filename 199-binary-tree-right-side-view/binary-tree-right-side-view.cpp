6af8d00c45811db0469f4c5b76d12743aac806ee
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-06-22 12:14:50 +0530

    Time: 3 ms (57.31%) | Memory: 13.9 MB (89.18%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -12,10 +12,14 @@
 class Solution {
 public:
     void helper(TreeNode* node, vector<int>& ans, int level) {
-        
+        //Reverse Preorder
         if (node == NULL) return;
-        if (level == ans.size()) ans.push_back(node->val);
+        if (level == ans.size()) ans.push_back(node->val); // As it travels the left subtree it will check that already that level is travelled in right subtree
         helper(node->right, ans, level + 1);
+        //we traverse the left subtree in addition to the right subtree is to ensure 
+        //we capture the rightmost nodes at each level. If a node does not have a right child, 
+        //the left child (if it exists) will be the rightmost node visible at that level. 
+        //Thus, to cover all cases, we need to process both subtrees.
         helper(node->left, ans, level + 1);
     }
     
