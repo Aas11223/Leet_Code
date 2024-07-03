3d2744475772f7a2050b04f228071e3ea2074372
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-07-03 09:53:34 +0530

    Time: 22 ms (46.45%) | Memory: 27.4 MB (40.23%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
-    // void rev(vector<int>& nums, int start, int end){
-    //     while(start<end){
-    //         swap(nums[start],nums[end]);
-    //         start++;
-    //         end--;
-    //     }
-    // }
-    void helper(vector<int>& nums, int start, int end){
+    void rev(vector<int>& nums, int start, int end){
         while(start<end){
             swap(nums[start],nums[end]);
             start++;
             end--;
         }
     }
-    
     void rotate(vector<int>& nums, int k){
-        // k=k%nums.size();
-        // // Since done rotate array to left so the Q is of right 
-        // rev(nums,0,nums.size()-1);// So rotate it to right  
-        // rev(nums,0,k-1); // apply now same logic for did in left 
-        // rev(nums,k,nums.size()-1);
         k=k%nums.size();
-        helper(nums,0,nums.size()-1);
-        helper(nums,0,k-1);
-        helper(nums,k,nums.size()-1);
+        // Since done rotate array to left so the Q is of right 
+        rev(nums,0,nums.size()-1);// So rotate it to right  
+        rev(nums,0,k-1); // apply now same logic for did in left 
+        rev(nums,k,nums.size()-1);
     }
 };
