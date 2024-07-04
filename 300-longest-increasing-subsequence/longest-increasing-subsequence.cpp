9db722edb9a64e7422fd55d935f6926149cae205
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-07-04 11:21:13 +0530

    Time: 74 ms (61.59%) | Memory: 12.8 MB (83.78%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -8,11 +8,5 @@ public:
                 if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
                     dp[i] = dp[j] + 1;
         return *max_element(dp.begin(), dp.end());
-        // vector<int> dp;
-        // for(int i=0;i<nums.size();i++){
-        //     for(int j=0;j<i;j++){
-
-        //     }
-        // }
     }
 };
