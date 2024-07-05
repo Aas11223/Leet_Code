429801cac4ba343be6fbc91fc2aff1641e71b6b3
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-07-05 11:41:00 +0530

    Time: 3 ms (41.67%) | Memory: 7.6 MB (60.94%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -17,15 +17,27 @@ public:
         //     }
         // }
         // return st.empty();
+        // stack<char> st;
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]=='(' || s[i]=='[' || s[i]=='{') st.push(s[i]);
+        //     else{
+        //         if(!st.empty()){
+        //             if((s[i]==')' && st.top()=='(') || (s[i]==']' && st.top()=='[') || (s[i]=='}' && st.top()=='{')) st.pop();
+        //             else return false;
+        //         }else return false;
+        //     }
+        // }
+        // return st.empty();
         stack<char> st;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{') st.push(s[i]);
+            if(s[i]=='(' ||s[i]=='{' ||s[i]=='[') st.push(s[i]);
             else{
                 if(!st.empty()){
-                    if((s[i]==')' && st.top()=='(') || (s[i]==']' && st.top()=='[') || (s[i]=='}' && st.top()=='{')) st.pop();
+                    if( s[i]==')' && st.top()=='(' || s[i]=='}' && st.top()=='{' || s[i]==']' && st.top()=='[') st.pop();
                     else return false;
                 }else return false;
             }
+            
         }
         return st.empty();
     }
