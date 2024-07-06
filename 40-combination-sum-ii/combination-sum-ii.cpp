d274685ced2b3c02043661b00408183c807001f8
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-07-06 10:46:57 +0530

    Time: 3 ms (78.90%) | Memory: 12.7 MB (56.47%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void helper(vector<int>& candidates, int target, int ind,vector<int> temp){
-        if(target==0){
-            ans.push_back(temp);
-            return;
-        }
-        for(int i=ind;i<candidates.size();i++){
-            if(candidates[i]>target) break; // Don't move ahead
-            if(i>ind && candidates[i]==candidates[i-1]) continue;
+//     vector<vector<int>> ans;
+//     void helper(vector<int>& candidates, int target, int ind,vector<int> temp){
+//         if(target==0){
+//             ans.push_back(temp);
+//             return;
+//         }
+//         for(int i=ind;i<candidates.size();i++){
+//             if(candidates[i]>target) break; // Don't move ahead
+//             if(i>ind && candidates[i]==candidates[i-1]) continue;
 
-            temp.push_back(candidates[i]);
-            helper(candidates,target-candidates[i],i+1,temp);
-            temp.pop_back(); 
-        }
+//             temp.push_back(candidates[i]);
+//             helper(candidates,target-candidates[i],i+1,temp);
+//             temp.pop_back(); 
+//         }
+//     }
+//     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+//         vector<int> temp;
+//         sort(candidates.begin(),candidates.end());
+//         helper(candidates,target,0,temp);
+//         return ans;
+//     }
+// };
+void helper(vector<int>& candidates, int target,int index,vector<vector<int>>& ans, vector<int>& curr){
+    if(target==0){
+        ans.push_back(curr);
+        return;
     }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int> temp;
-        sort(candidates.begin(),candidates.end());
-        helper(candidates,target,0,temp);
-        return ans;
+    if(target<0) return;
+    for(int i=index;i<candidates.size();i++){
+        if(candidates[i]>target) break;
+        if(i>index && candidates[i]==candidates[i-1]) continue;
+        curr.push_back(candidates[i]);
+        helper(candidates,target-candidates[i],i+1,ans,curr);
+        curr.pop_back();
     }
+}
+vector<vector<int>> combinationSum2(vector<int>& candidates, int target){
+    vector<vector<int>> ans;
+    vector<int> curr;
+    sort(candidates.begin(),candidates.end());
+    helper(candidates,target,0,ans,curr);
+    return ans;
+}
 };
