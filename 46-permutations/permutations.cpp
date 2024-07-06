fcd157d6daccbf3e6aede68276f0de9513c0f6aa
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-07-06 11:12:53 +0530

    Time: 5 ms (32.98%) | Memory: 9.1 MB (52.80%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -16,17 +16,5 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         helper(nums,0);
         return ans;
-    // void helper(vector<int>& nums,int index,vector<vector<int>>& ans){
-    //     if(index==nums.size()) ans.push_back(nums);
-    //     for(int i=index;i<nums.size();i++){
-    //         swap(nums[index],nums[i]);
-    //         helper(nums,index+1,ans);
-    //         swap(nums[index],nums[i]);
-    //     }
-    // }
-    // vector<vector<int>> permute(vector<int>& nums) {
-    //     vector<vector<int>> ans;
-    //     helper(nums,0,ans);
-    //     return ans;
     }
 };
