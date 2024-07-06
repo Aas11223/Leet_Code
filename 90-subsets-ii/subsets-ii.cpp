86e40e3797dac630550354328da136edbbaaa39e
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-07-06 10:02:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (83.10%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,34 +1,7 @@
 class Solution {
 public:
-//     // void helper (vector<int> nums,int index,vector<int> &ans){
-//     //     if(index==nums.size()){
-//     //         ans.push_back(nums[index]);
-//     //         return;
-//     //     }
-//     //     helper(nums[index]+nums[index+1], index+1,ans);
-//     //     helper(nums[index], index+1,ans);
-//     // }
-//     void helper(vector<int>& nums, int index,vector<int>& curr, vector<vector<int>> & res){
-//         res.push_back(curr);
-//         for(int i=0;i<nums.size();i++){
-//             if(i>index && nums[i]==nums[i-1]) continue;
-//             curr.push_back(nums[i]);
-//             helper(nums,i+1,curr,res);
-//             curr.pop_back();
-//         }
-        
-//     }
-//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-//         // vector<int> ans;
-//         // helper(nums,0,ans);
-//         vector<int> curr;
-//         vector<vector<int>> res;
-//         helper(nums,0,curr,res);
-//         return res;
-//     }
-// };
-
 void helper(vector<int>& nums, int index, vector<int>& current, vector<vector<int>>& result) {
+    //vector<vector<int>>& result means vector inside a vector
     // Add the current subset to the result
     result.push_back(current);
 
