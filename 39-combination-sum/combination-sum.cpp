0ddba149cac932fa111a8a8cf6a376d0d9ac6023
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-07-06 10:26:32 +0530

    Time: 4 ms (72.85%) | Memory: 12.2 MB (84.42%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -14,27 +14,47 @@ public:
     //     temp.pop_back();
     //     helper(candidates,target,i+1,currSum,temp);//Exclusion
     // }
-    void combinationsum(vector<int>& candidates,int target,vector<int>&v,vector<vector<int>>&ans,int index){
+    // void combinationsum(vector<int>& candidates,int target,vector<int>&v,vector<vector<int>>&ans,int index){
+    //     if(target==0){
+    //         ans.push_back(v);
+    //         return;
+    //     }
+    //     if(target<0){
+    //         return;
+    //     }
+    //     for(int i=index;i<candidates.size();i++){
+    //         v.push_back(candidates[i]);
+    //         combinationsum(candidates,target-candidates[i],v,ans,i);
+    //         v.pop_back();
+    //     }
+    // }
+    // vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    //     // vector<int> temp;
+    //     // helper(candidates,target,0,0,temp);
+    //     // return ans;
+    //     vector<vector<int>>ans;
+    //     vector<int>v;
+    //     combinationsum(candidates,target,v,ans,0);
+    //     return ans;
+    // }
+    void helper(vector<int>& candidates, int target,int index,vector<vector<int>>& ans,vector<int>& curr){
         if(target==0){
-            ans.push_back(v);
+            ans.push_back(curr);
             return;
         }
         if(target<0){
             return;
         }
         for(int i=index;i<candidates.size();i++){
-            v.push_back(candidates[i]);
-            combinationsum(candidates,target-candidates[i],v,ans,i);
-            v.pop_back();
+            curr.push_back(candidates[i]);
+            helper(candidates,target-candidates[i],i,ans,curr);
+            curr.pop_back();
         }
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        // vector<int> temp;
-        // helper(candidates,target,0,0,temp);
-        // return ans;
-        vector<vector<int>>ans;
-        vector<int>v;
-        combinationsum(candidates,target,v,ans,0);
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target){
+        vector<vector<int>> ans;
+        vector<int> curr;
+        helper(candidates,target,0,ans,curr);
         return ans;
     }
 };
