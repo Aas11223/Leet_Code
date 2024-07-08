001c2b1b19e8ceeb2921fb8a2b3e668d5ea553c6
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-07-08 13:58:45 +0530

    Time: 62 ms (73.88%) | Memory: 12.9 MB (54.48%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,12 +1,20 @@
 class Solution { // 256 ms, faster than 42.84%
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, 1);
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < i; j++)
-                if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
-                    dp[i] = dp[j] + 1;
-        return *max_element(dp.begin(), dp.end());
+        // int n = nums.size();
+        // vector<int> dp(n, 1);
+        // for (int i = 0; i < n; i++)
+        //     for (int j = 0; j < i; j++)
+        //         if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
+        //             dp[i] = dp[j] + 1;
+        // return *max_element(dp.begin(), dp.end());
+        vector<int> dp(nums.size(),1);
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]) dp[i]=max(dp[i],dp[j]+1);
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
     }
 };
+
