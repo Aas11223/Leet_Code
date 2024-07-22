081c9132184df4d1c7bcfbbfb90ca1c1e76c95f6
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-07-22 11:44:32 +0530

    Time: 3 ms (45.39%) | Memory: 7.8 MB (31.09%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string res="";
+        // string res="";
+        // int i=0;
+        // int j=0;
+        // while(i<word1.size() || j<word2.size()){
+        //     if(i<word1.size()) res+=word1[i++];
+
+        //     if(j<word2.size()) res+=word2[j++];
+        // }
+        // return res;
+        string ans="";
         int i=0;
         int j=0;
         while(i<word1.size() || j<word2.size()){
-            if(i<word1.size()) res+=word1[i++];
-
-            if(j<word2.size()) res+=word2[j++];
+            if(i<word1.size()) ans+=word1[i++];
+            if(j<word2.size()) ans+=word2[j++];
         }
-        return res;
+        return ans;
     }
 };
