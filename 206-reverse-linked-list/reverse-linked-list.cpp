3140b3395767e1b39df9fc30be3b96e9414cf370
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-07-23 12:07:15 +0530

    Time: 4 ms (60.71%) | Memory: 13.2 MB (6.98%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -25,11 +25,16 @@ public:
         // return prev;
 
         //Recursive approach
+        // if(head==NULL || head->next==NULL) return head;
+        // ListNode* rest=reverseList(head->next);
+        // head->next->next=head;
+        // head->next=NULL;
+        // return rest;
+        
         if(head==NULL || head->next==NULL) return head;
         ListNode* rest=reverseList(head->next);
         head->next->next=head;
         head->next=NULL;
         return rest;
-
     }
 };
