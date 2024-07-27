6391c1b6935b09f6df2412e9dcae3f1a23680df5
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-07-27 18:20:37 +0530

    Time: 82 ms (68.83%) | Memory: 114.4 MB (24.58%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -9,8 +9,9 @@ public:
         int first=INT_MAX;
         int second=INT_MAX;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]<=first) first=nums[i];
-            else if(nums[i]<=second) second=nums[i];
+            if(nums[i]<=first) first=nums[i]; // Update first if num is smaller or equal
+            else if(nums[i]<=second) second=nums[i]; // Update second if num is smaller or equal
+            // If num is greater than both first and second, we have an increasing triplet
             else return true;
         }
         return false;
