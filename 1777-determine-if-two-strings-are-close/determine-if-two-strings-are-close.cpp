9be78324bfae7e4f892ec773801944ae999e630b
Aastha-Gadhvi <102249405+Aastha-Gadhvi@users.noreply.github.com>
2024-07-29 14:27:30 +0530

    Time: 67 ms (65.58%) | Memory: 22.2 MB (56.24%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
+        //Not same length
         if(word1.size()!=word2.size()) return false;
         vector<int>f1(26,0);
         vector<int>f2(26,0);
+        //Calculate frequency
         for(int i=0;i<word1.size();i++) f1[word1[i] - 'a']++;
         for(int i=0;i<word2.size();i++) f2[word2[i] - 'a']++;
-        //if(f1.size() != f2.size()) return false;
+        //Not same set of character (i.e in word1 have letter 's' but in word2 it don't have 's')
         for(int i=0;i<26;i++){
             if(f1[i]==0 && f2[i]!=0 || f1[i]!=0 && f2[i]==0) return false;
         }
         sort(f1.begin(),f1.end());
         sort(f2.begin(),f2.end());
+        //Not same frequency
         for(int i=0;i<26;i++) if(f1[i]!=f2[i]) return false;
         return true;
     }
